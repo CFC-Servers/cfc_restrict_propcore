412e915f534952b6d6d405c8deee5016e0de5ca6
Claydo9 <54473393+Claydo9@users.noreply.github.com>
2020-02-13 17:00:01 +1000

    Restricted entity:use()
    Restricted the propcore function entity:use()

--- lua/autorun/server/sv_restrict_propcore.lua
+++ lua/autorun/server/sv_restrict_propcore.lua
@@ -4,6 +4,7 @@ function restrictPropCoreFunctions()
     local disallowedRanks = {}
     disallowedRanks["user"] = true
     disallowedRanks["regular"] = true
+    
 
     local restrictedFunctions = {
         "propSpawn(sn)",
@@ -21,13 +22,38 @@ function restrictPropCoreFunctions()
         "propManipulate(e:vannn)"
         --"propBreak(e:)"
     }
+    
+    local globalrestrictedFunctions = {
+        "use(e:)"   
+    }
 
+    
     for _, signature in pairs( restrictedFunctions ) do
         if wire_expression2_funcs then
             local oldFunc = wire_expression2_funcs[signature][3]
 
             wire_expression2_funcs[signature][3] = function( self, ... )
                 if ( disallowedRanks[self.player:GetUserGroup()] == nil ) then
+                    local isInBuildMode = self.player:GetNWBool("CFC_PvP_Mode", false) == false
+
+                    if( isInBuildMode or self.player:IsAdmin() ) then
+                        return oldFunc( self, ... )
+                    else
+                        self.player:ChatPrint( "You can't use PropCore in PvP mode" )
+                    end
+                else
+                    self.player:ChatPrint( "You don't have access to " .. signature )
+                end
+            end
+        end
+    end
+    
+    for _, signature in pairs( globalrestrictedFunctions ) do
+        if wire_expression2_funcs then
+            local oldFunc = wire_expression2_funcs[signature][3]
+
+            wire_expression2_funcs[signature][3] = function( self, ... )
+                if ( self.player:IsAdminI() ) then
                     local isInBuildMode = self.player:GetNWBool("CFC_PvP_Mode", false) == false
 
                     if( isInBuildMode or self.player:IsAdmin() ) then
